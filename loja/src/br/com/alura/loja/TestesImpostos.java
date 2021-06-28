aafd67e7227e2d4101e2ef2ffd62feaf000896a5
Leandro Moreira <leandrodev.oliveira@gmail.com>
2021-06-27 21:31:22 -0300

    Projeto criado

---
+++ loja/src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,20 @@
+package br.com.alura.loja;
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.imposto.Imposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImpostos {
+
+	//Padrão Strategy
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		System.out.println(calculadora.calcular(orcamento, new ICMS()));
+
+	}
+
+}
